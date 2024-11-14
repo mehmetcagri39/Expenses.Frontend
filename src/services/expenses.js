52d3afd6b4311cbf4e30e39d8800e473a34a8ed1
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2024-11-14 12:41:41 +0300

    Expenses projesi oluşturuldu.

---
+++ src/services/expenses.js
@@ -0,0 +1,17 @@
+export const GetExpenses = async (dispatch) => {
+    try {
+        const expenses = [
+            {
+                id:1, description: 'Groceries', amount: 23.16
+            },
+            {
+                id:2, description: 'Gas', amount: 18.52
+            },
+            {
+                id:3, description: 'Student Loans', amount: 550
+            }
+        ];
+    } catch {
+        console.log("Error!")
+    }
+}
